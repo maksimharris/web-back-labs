bc01fa47f664dc56fa90eba277dd8a62f9b19861
Писарев Максим <maksim.pisarev.1986@maill.ru>
2025-09-12 02:19:40 +0700

    Перенаправление

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, request
+from flask import Flask, url_for, request, redirect
 import datetime
 app = Flask(__name__)
 
@@ -56,3 +56,6 @@ def counter():
     </body>
 <html>
 '''
+@app.route("/info")
+def info():
+    return redirect("/author")
