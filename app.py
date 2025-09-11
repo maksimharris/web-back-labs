f3cb2c94ff62c00b9b2efd29da9e4524d72572a5
Писарев Максим <maksim.pisarev.1986@maill.ru>
2025-09-12 01:09:28 +0700

    Информация об авторе

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def start():
+@app.route("/web")
+def web():
     return """<!doctype html> 
         <html> 
             <body>
                 <h1>web-сервер на flask</h1>
+                <a href ="/author">author</a>
+            </body>
+        </html>"""
+@app.route("/author")
+def author():
+    name = "Писарев Максим Иванович"
+    group = "ФБИ-31"
+    faculty = "ФБ"
+    return """<!doctype html>
+        <html>
+            <body>
+                <p>Студент: """ + name + """</p>
+                <p>Группа: """ + group + """</p>
+                <p>Факультет: """+ faculty + """</p>
+                <a href ="/web">web</a>
             </body>
         </html>"""
