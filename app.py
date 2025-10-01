834a92057b25a668e539725a9bc2ba07b3ccbb1a
Максим Писарев <maksim.pisarev.1986@mail.ru>
2025-10-01 11:10:54 +0700

    Имя студента

--- app.py
+++ app.py
@@ -307,4 +307,5 @@ def add_flower(name):
 
 @app.route('/lab2/example')
 def example():
-    return render_template('example.html')
+    name = 'Максим Писарев'
+    return render_template('example.html',name=name)
