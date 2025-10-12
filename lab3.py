d39a6a3022b67c8ae74c5f3f841c1aa405fa61f2
Максим Писарев <maksim.pisarev.1986@mail.ru>
2025-10-12 14:08:47 +0700

    Валидация

--- lab3.py
+++ lab3.py
@@ -24,7 +24,15 @@ def del_cookies():
     return resp
 @lab3.route('/lab3/form1')
 def form1():
+    errors = {}
     user = request.args.get('user')
+    if user == '':
+        errors['user'] = 'Заполнить поле!'
     age = request.args.get('age')
+    if age == '':
+        errors['age'] = 'Заполнить поле!'
     sex = request.args.get('sex')
-    return render_template('/lab3/form1.html',user = user,age=age,sex=sex)
+    return render_template('/lab3/form1.html',user = user,
+                                            age=age,
+                                            sex=sex,
+                                            errors = errors)
