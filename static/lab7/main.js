cb810c10590b8a8a495cbbe3f7d2ee01e30fa9ac
Максим Писарев <maksim.pisarev.1986@mail.ru>
2025-12-10 16:17:37 +0700

    Редактирование фильмов

--- static/lab7/main.js
+++ static/lab7/main.js
@@ -20,6 +20,9 @@ function fillFilmList() {
 
             let editButton = document.createElement('button');
             editButton.innerText = 'редактировать';
+            editButton.onclick = function(){
+                editFilm(i);
+            };
 
             let delButton = document.createElement('button');
             delButton.innerText = 'удалить';
@@ -79,8 +82,8 @@ function sendFilm() {
         year: document.getElementById('year').value,
         description: document.getElementById('description').value
     }
-    const url = `/lab7/rest-api/films/`;
-    const method = 'POST';
+    const url = `/lab7/rest-api/films/${id}`;
+    const method = id === '' ? 'POST':'PUT';
     fetch(url, {
         method: method,
         headers: {"Content-Type":"application/json"},
@@ -90,4 +93,18 @@ function sendFilm() {
         fillFilmList();
         hideModal();
     });
+}
+function editFilm(id){
+    fetch(`/lab7/rest-api/films/${id}`)
+    .then(function(data) {
+        return data.json();
+    })
+    .then(function (film) {
+        document.getElementById('id').value = id;
+        document.getElementById('title').value = film.title;
+        document.getElementById('title-ru').value = film.title_ry;
+        document.getElementById('year').value = film.year;
+        document.getElementById('description').value = film.description;
+        showModal();
+    })
 }
