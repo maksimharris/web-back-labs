01e90051e9024ba845da9d0fd9c6e428264affe0
Максим Писарев <maksim.pisarev.1986@mail.ru>
2025-12-10 18:23:06 +0700

    Исправление

--- lab7.py
+++ lab7.py
@@ -21,7 +21,7 @@ def db_connect():
     else:
         # SQLite для локальной разработки
         dir_path = path.dirname(path.realpath(__file__))
-        db_path = path.join(dir_path, "database.db")
+        db_path = path.join(dir_path, "database2.db")
         conn = sqlite3.connect(db_path)
         conn.row_factory = sqlite3.Row
         cur = conn.cursor()
