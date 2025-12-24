dad8191dbd0699d3c4f2ed612717801e1d1ac29e
Максим Писарев <maksim.pisarev.1986@mail.ru>
2025-12-24 16:03:46 +0700

    Модель базы данных

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ import datetime
 
 from flask import Flask, url_for, request, redirect, abort, render_template, current_app
 import os
+from os import path
+from flask_sqlalchemy import SQLAlchemy
+
+from db import db
 from dotenv import load_dotenv  # Добавьте эту строку
 
 load_dotenv()
@@ -26,7 +30,23 @@ from rgz import rgz
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'ыускуе_лун')
-app.config['DB_TYPE'] = os.environ.get('DB_TYPE','postgres')
+app.config['DB_TYPE'] = os.getenv('DB_TYPE', 'postgres')
+
+if app.config['DB_TYPE'] == 'postgres':
+    db_name = 'maxim_pisarev_orm'
+    db_user = 'maxim_pisarev_orm'
+    db_password = '123'
+    host_ip = '127.0.0.1'
+    host_port = 5432
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = \
+        f'postgresql://{db_user}:{db_password}@{host_ip}:{host_port}/{db_name}'
+else:
+    dir_path = path.dirname(path.realpath(__file__))
+    db_path = path.join(dir_path, "maxim_pisarev_orm.db")
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
+
+db.init_app(app)
 
 
 app.register_blueprint(lab1)
--- lab8.py
+++ lab8.py
@@ -4,7 +4,8 @@ from psycopg2.extras import RealDictCursor
 from werkzeug.security import check_password_hash, generate_password_hash
 import sqlite3
 from os import path
-
+from db.models import users, articles
+from db import db
 lab8 = Blueprint('lab8', __name__)
 
 @lab8.route('/lab8/')
